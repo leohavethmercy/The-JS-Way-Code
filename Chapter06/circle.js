f4314a993776b9aa24259fc6f3e5f77a11132e84
leohavethmercy <billiejefferys@gmail.com>
2022-03-19 19:22:56 -0400

    Completed circle area exercise

---
+++ Chapter06/circle.js
@@ -0,0 +1,16 @@
+//Modeling a circle
+//Complete the following program to add the circle object definition. 
+//Its radius value is input by the user.
+
+const r = 5
+// Number(prompt("Enter the circle radius:"));
+
+// TODO: create the circle object here
+
+const circle = {
+    circumference() { return 2*Math.PI*r },
+    area() {return Math.PI*Math.pow(r,2)}
+}
+
+console.log(`Its circumference is ${circle.circumference()}`);
+console.log(`Its area is ${circle.area()}`);
